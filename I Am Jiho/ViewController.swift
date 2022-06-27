9f9d73afb5f8a253b472ba51024cc5e46065456e
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2022-06-28 07:48:26 +0900

    first commit : basic

---
+++ I Am Jiho/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Jiho
+//
+//  Created by 엄지호 on 2022/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
